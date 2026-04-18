f04b1637f5110480ce5d5ebac5331ea668c7ba36
FrozenMetroid <frozenmetroid7@gmail.com>
2026-04-17 20:56:54 -0400

    Update Summary Screen Icon Palette Indices

--- res/field/scripts/scripts_jubilife_city.s
+++ res/field/scripts/scripts_jubilife_city.s
@@ -1620,7 +1620,6 @@ JubilifeCity_YouShouldStickAround:
     End
 
 JubilifeCity_KidWithNDS1:
-    // GivePokemon SPECIES_NIDORINA, 1, 0, VAR_RESULT
     NPCMessage JubilifeCity_Text_ImTradingPokemonWithMyBuddy
     End
 
